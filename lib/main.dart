6a0f0da5926daa1b70255452197c042f6d45dcba
acannie <acannie0530@gmail.com>
2021-05-05 01:51:05 +0900

    code: deleted comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,20 @@
-// import 'package:flutter/material.dart';
-
-// import 'view.dart';
-
-// void main() => runApp(const MyApp());
-
-// /// This is the main application widget.
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   static const String _title = 'Acannie\'s HomePage';
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: _title,
-//       home: MyHomePage(),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
+
 import 'view.dart';
 
-void main() {
-  runApp(new MyApp());
-}
+void main() => runApp(const MyApp());
 
+/// This is the main application widget.
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Acannie\'s HomePage';
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      home: new MyHomePage(),
+    return MaterialApp(
+      title: _title,
+      home: MyHomePage(),
     );
   }
 }
