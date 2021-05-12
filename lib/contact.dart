00803e60d860c25836d1e41d0b432f73170b0b96
acannie <acannie0530@gmail.com>
2021-05-13 01:11:05 +0900

    code: sns logo and name

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,10 +1,77 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-// コンタクト
+import 'utils.dart';
+import 'layout.dart';
+
+// 自己紹介
 class Contact extends StatelessWidget {
+  List<Map<String, String>> snsAccount = [
+    {'imagePath': 'assets/facebook.png', 'snsName': 'Facebook', 'url': ''},
+    {'imagePath': 'assets/github.png', 'snsName': 'GitHub', 'url': ''},
+    {'imagePath': 'assets/linkedin.png', 'snsName': 'LinkedIn', 'url': ''},
+    {'imagePath': 'assets/twitter.png', 'snsName': 'Twitter', 'url': ''},
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Text("Coming Soon!");
+    Widget snsWithLogo(String imagePath, String snsName) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 30.0,
+            height: 30.0,
+            decoration: new BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(imagePath),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(10)),
+          Layout.sentenceText(snsName),
+        ],
+      );
+    }
+
+    return Container(
+      child: Column(
+        children: [
+          Padding(padding: EdgeInsets.all(30)),
+          Layout.titleText('Contact'),
+          Padding(padding: EdgeInsets.all(20)),
+          for (int i = 0; i < snsAccount.length; i++)
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 30.0,
+                      height: 30.0,
+                      decoration: new BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(snsAccount[i]['imagePath']!),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.all(10)),
+                    Layout.sentenceText(snsAccount[i]['snsName']!),
+                  ],
+                ),
+                Padding(padding: EdgeInsets.all(20)),
+              ],
+            ),
+          // Layout.historyTable(educationalBackground),
+          Padding(padding: EdgeInsets.all(20)),
+          // Layout.historyTable(workHistory),
+          Padding(padding: EdgeInsets.all(30)),
+        ],
+      ),
+    );
   }
 }
