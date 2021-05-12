52b83fe3f65f6a3c1dc64f5fc41d13dd38472123
acannie <acannie0530@gmail.com>
2021-05-13 01:33:58 +0900

    code: url linked with button

--- lib/contact.dart
+++ lib/contact.dart
@@ -2,18 +2,43 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'utils.dart';
 import 'layout.dart';
 
 // 自己紹介
 class Contact extends StatelessWidget {
-  List<Map<String, String>> snsAccount = [
-    {'imagePath': 'assets/facebook.png', 'snsName': 'Facebook', 'url': ''},
-    {'imagePath': 'assets/github.png', 'snsName': 'GitHub', 'url': ''},
-    {'imagePath': 'assets/linkedin.png', 'snsName': 'LinkedIn', 'url': ''},
-    {'imagePath': 'assets/twitter.png', 'snsName': 'Twitter', 'url': ''},
+  List<Map<String, String>> snsAccounts = [
+    {
+      'imagePath': 'assets/facebook.png',
+      'name': 'Facebook',
+      'url': 'https://www.facebook.com/made.in.akane'
+    },
+    {
+      'imagePath': 'assets/github.png',
+      'name': 'GitHub',
+      'url': 'https://github.com/acannie'
+    },
+    {
+      'imagePath': 'assets/linkedin.png',
+      'name': 'LinkedIn',
+      'url': 'https://www.linkedin.com/in/akane-sasaoka/'
+    },
+    {
+      'imagePath': 'assets/twitter.png',
+      'name': 'Twitter',
+      'url': 'https://twitter.com/CO_KEISAN_SKILL'
+    },
   ];
+
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,25 +68,15 @@ class Contact extends StatelessWidget {
           Padding(padding: EdgeInsets.all(30)),
           Layout.titleText('Contact'),
           Padding(padding: EdgeInsets.all(20)),
-          for (int i = 0; i < snsAccount.length; i++)
+          for (var snsAccount in snsAccounts)
             Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 30.0,
-                      height: 30.0,
-                      decoration: new BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage(snsAccount[i]['imagePath']!),
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                    Padding(padding: EdgeInsets.all(10)),
-                    Layout.sentenceText(snsAccount[i]['snsName']!),
-                  ],
+                InkWell(
+                  onTap: () => _launchURL(snsAccount['url']!),
+                  child: snsWithLogo(
+                    snsAccount['imagePath']!,
+                    snsAccount['name']!,
+                  ),
                 ),
                 Padding(padding: EdgeInsets.all(20)),
               ],
