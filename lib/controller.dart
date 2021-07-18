974d494e1e964ba65bd98ffe5eccf58c89c716ad
acannie <acannie0530@gmail.com>
2021-07-19 01:03:12 +0900

    code: ページ情報をclass化。

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,8 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'introduce.dart';
+import 'contact.dart';
+import 'works.dart';
+import 'engineering.dart';
+import 'favorite.dart';
+
+// Page
+class PageContent {
+  IconData iconData;
+  Color iconColor;
+  String title;
+  Widget content;
+
+  PageContent({
+    required this.iconData,
+    required this.iconColor,
+    required this.title,
+    required this.content,
+  });
+}
+
 class AcannieController with ChangeNotifier {
   // 定数
+  List<PageContent> pageContents = [
+    PageContent(
+      iconData: Icons.sentiment_satisfied_outlined,
+      iconColor: Color.fromARGB(255, 81, 154, 186),
+      title: "Introduce",
+      content: Introduce(),
+    ),
+    PageContent(
+      iconData: Icons.contact_mail,
+      iconColor: Color.fromARGB(255, 160, 116, 196),
+      title: "Contact",
+      content: Contact(),
+    ),
+    PageContent(
+      iconData: Icons.palette,
+      iconColor: Color.fromARGB(255, 227, 121, 51),
+      title: "Works",
+      content: Works(),
+    ),
+    PageContent(
+      iconData: Icons.computer,
+      iconColor: Color.fromARGB(255, 81, 154, 186),
+      title: "Engineering",
+      content: Engineering(),
+    ),
+    PageContent(
+      iconData: Icons.favorite,
+      iconColor: Color.fromARGB(255, 204, 62, 68),
+      title: "Favorite",
+      content: Favorite(),
+    ),
+  ];
+
   // 変数
   int get activePageIndex => _activePageIndex;
   int _activePageIndex = 0;
