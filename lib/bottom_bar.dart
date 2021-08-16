ffb0d8f3647a39e2264d1fdd79f351359b4a915a
acannie <acannie0530@gmail.com>
2021-08-16 10:05:47 +0900

    code: WSL のボタンを実装。

--- lib/bottom_bar.dart
+++ lib/bottom_bar.dart
@@ -12,21 +12,42 @@ class BottomBar extends StatelessWidget {
 
     return BottomAppBar(
       child: Container(
-        height: 20.0,
+        height: 22,
         child: Row(
           children: [
+            // WSL のボタン
             Container(
-              width: 140,
-              alignment: Alignment.centerLeft,
+              alignment: Alignment.center,
               color: Layout.bottomBarRemoteBg,
-              child: Text(
-                ">< WSL: Ubuntu-20.04",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 17,
-                ),
-              ),
+              child: InkWell(
+                  child: Row(
+                    children: [
+                      Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
+                      Icon(
+                        Icons.compare_arrows,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                      Padding(padding: EdgeInsets.symmetric(horizontal: 1)),
+                      Visibility(
+                        visible: _controller.wslMode,
+                        child: Text(
+                          "WSL: Ubuntu-20.04",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.symmetric(horizontal: 7)),
+                    ],
+                  ),
+                  onTap: () {
+                    _controller.switchWslMode();
+                  }),
             ),
+
+            // メインのバー
             Expanded(
               child: Container(
                 alignment: Alignment.centerLeft,
